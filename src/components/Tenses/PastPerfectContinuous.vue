79f8a54697b1c5d114c6510b223e5997408eb23d
Serhii Vozniuk <serega.voznyuk@gmail.com>
2023-01-03 17:51:16 +0200

    adding new tensens

---
+++ src/components/Tenses/PastPerfectContinuous.vue
@@ -0,0 +1,104 @@
+<template>
+    <div v-if="description" v-html="description" class="description"></div>
+    <div class="table">
+        <div class="div1">Tense</div>
+        <div class="div2">Form</div>
+        <div class="div3">Use</div>
+        <div class="div16">Signal words</div>
+        <div class="div4">
+            Past Perfect
+        </div>
+        <div class="div5">
+            had + <span class="inf">past participle*</span><span class="inf w100">(3nd irregular verbs)</span>
+        </div>
+        <div class="div6">
+            <table class="ec-table">
+                <thead>
+                <tr>
+                    <th>&nbsp;</th>
+                    <th><strong>subject</strong></th>
+                    <th class="bg-accent-1"><strong>auxiliary verb</strong></th>
+                    <th>&nbsp;</th>
+                    <th class="bg-accent-3"><strong>main verb</strong></th>
+                    <th>&nbsp;</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th rowspan="2">+</th>
+                    <td>I, you, we, they</td>
+                    <td class="bg-accent-1">had</td>
+                    <td> </td>
+                    <td class="bg-accent-3">finished</td>
+                    <td>my work.</td>
+                </tr>
+                <tr>
+                    <td>He, she, it</td>
+                    <td class="bg-accent-1">had</td>
+                    <td></td>
+                    <td class="bg-accent-3">eaten</td>
+                    <td>before me.</td>
+                </tr>
+                <tr>
+                    <th rowspan="2">-</th>
+                    <td>I, you, we, they</td>
+                    <td class="bg-accent-1">had</td>
+                    <td>not</td>
+                    <td class="bg-accent-3">gone</td>
+                    <td>to school.</td>
+                </tr>
+                <tr>
+                    <td>He, she, it</td>
+                    <td class="bg-accent-1">had</td>
+                    <td>not</td>
+                    <td class="bg-accent-3">left.</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th rowspan="2">?</th>
+                    <td class="bg-accent-1">Had</td>
+                    <td>I, you, we, they</td>
+                    <td>&nbsp;</td>
+                    <td class="bg-accent-3">arrived?</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td class="bg-accent-1">Had</td>
+                    <td>he, she, it</td>
+                    <td>&nbsp;</td>
+                    <td class="bg-accent-3">eaten</td>
+                    <td>dinner?</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="div9 use">
+            <ul>
+                <li>mostly when two actions in a story are related to each other: the action which had already happened is put into Past Perfect, the other action into Simple Past</li>
+                <li>the past of Present Perfect</li>
+            </ul>
+        </div>
+        <div class="div17 signal-words">
+            <ul>
+                <li>already</li>
+                <li>just</li>
+                <li>never</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "PastPerfect",
+        props: {
+            description: {
+                type: String,
+            }
+        }
+    }
+</script>
+
+<style src="@/assets/grammar.style.css" scoped>
+
+</style>
---
+++ src/components/Tenses/PresentPerfectContinuous.vue
@@ -0,0 +1,107 @@
+<template>
+    <div v-if="description" v-html="description" class="description"></div>
+    <div class="table">
+        <div class="div1">Tense</div>
+        <div class="div2">Form</div>
+        <div class="div3">Use</div>
+        <div class="div16">Signal words</div>
+        <div class="div4">
+            Present Perfect Continuous
+        </div>
+        <div class="div5">
+            have/has + <span class="green">been</span>&nbsp;+&nbsp;<span class="inf">infinitive </span> &nbsp;+&nbsp;<span class="end">-ing </span><br/>
+        </div>
+        <div class="div6">
+            <table class="ec-table">
+                <thead>
+                <tr>
+                    <th>&nbsp;</th>
+                    <th><strong>subject</strong></th>
+                    <th class="bg-accent-1"><strong>auxiliary verb</strong></th>
+                    <th>&nbsp;</th>
+                    <th class="bg-accent-3"><strong>main verb</strong></th>
+                    <th>&nbsp;</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th rowspan="2">+</th>
+                    <td>I, you, we, they</td>
+                    <td class="bg-accent-1">have <span class="green">been</span></td>
+                    <td> </td>
+                    <td class="bg-accent-3">waiting</td>
+                    <td>for one hour.</td>
+                </tr>
+                <tr>
+                    <td>He, she, it</td>
+                    <td class="bg-accent-1">has <span class="green">been</span></td>
+                    <td></td>
+                    <td class="bg-accent-3">talking</td>
+                    <td>too much.</td>
+                </tr>
+                <tr>
+                    <th rowspan="2">-</th>
+                    <td>I, you, we, they</td>
+                    <td class="bg-accent-1">have</td>
+                    <td>not</td>
+                    <td class="bg-accent-3"><span class="green">been</span> going.</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>He, she, it</td>
+                    <td class="bg-accent-1">has</td>
+                    <td>not</td>
+                    <td class="bg-accent-3"><span class="green">been</span> playing</td>
+                    <td>football.</td>
+                </tr>
+                <tr>
+                    <th rowspan="2">?</th>
+                    <td class="bg-accent-1">Have</td>
+                    <td>I, you, we, they</td>
+                    <td>&nbsp;</td>
+                    <td class="bg-accent-3"><span class="green">been</span> seeing</td>
+                    <td>her?</td>
+                </tr>
+                <tr>
+                    <td class="bg-accent-1">Has</td>
+                    <td>he, she, it</td>
+                    <td>&nbsp;</td>
+                    <td class="bg-accent-3"><span class="green">been</span> doing</td>
+                    <td>their homework?</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="div9 use">
+            <ul>
+                <li>action began in the past and has just stopped</li>
+                <li>how long the action has been happening</li>
+                <li><b>emphasis: length of time of an action</b></li>
+            </ul>
+        </div>
+        <div class="div17 signal-words">
+            <ul>
+                <li>all day</li>
+                <li>the whole day</li>
+                <li>how long</li>
+                <li>since</li>
+                <li>for</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "PresentPerfectContinuous",
+        props: {
+            description: {
+                type: Array,
+            }
+        }
+    }
+</script>
+
+<style src="@/assets/grammar.style.css" scoped>
+
+</style>
